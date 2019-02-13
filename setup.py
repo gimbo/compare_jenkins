b6c4f4077b23791abbccfb0e3c11e3a6ab733b87
Andy Gimblett <andy@barefootcode.com>
2019-02-13 17:05:23 +0000

    Rename jen_compare entry point to jen-compare

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
     install_requires=install_requires,
     entry_points={
         'console_scripts': [
-            'jen_compare=jen_compare:main',
+            'jen-compare=jen_compare:main',
         ],
     },
 )
